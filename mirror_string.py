d8e221f4a8eda5a67e48efb8feac511c80bd66c3
BINDU MADHAV <76785889+Bindumadhav1507@users.noreply.github.com>
2022-11-17 23:24:38 +0530

    Add files via upload

---
+++ mirror_string.py
@@ -0,0 +1,7 @@
+########################### Mirror String #########################
+user = input("enter the charecter: ")
+string_reversed = ""
+
+for i in user:
+  string_reversed = i + string_reversed
+print("the reversed string is: " ,string_reversed)
